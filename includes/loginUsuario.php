7ba897b8e2acd11fdb72367b146ea76c90b7fb31
BombasticOri <max123ah@gmail.com>
2024-12-12 16:15:01 -0500

    V0.3.1

--- includes/loginUsuario.php
+++ includes/loginUsuario.php
@@ -10,6 +10,7 @@ if (!empty($_POST)) {
         $identificador = $_POST['identificador'];
         $password = $_POST['password'];
 
+        // Consulta para obtener el usuario y la contraseña
         $sql = 'SELECT * FROM mercado_gestion.usuarios AS u 
                 INNER JOIN mercado_gestion.roles AS r 
                 ON u.rol_id = r.rol_id 
@@ -19,8 +20,24 @@ if (!empty($_POST)) {
         $result = $query->fetch(PDO::FETCH_ASSOC);
 
         if ($query->rowCount() > 0) {
+            // Verificar si la contraseña necesita ser cifrada o rehashada
+            if (password_needs_rehash($result['password'], PASSWORD_DEFAULT)) {
+                // Si la contraseña no está cifrada o necesita ser rehashada, ciframos la nueva contraseña
+                $newPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                // Actualizamos la contraseña en la base de datos
+                $updateSql = 'UPDATE mercado_gestion.usuarios SET password = ? WHERE identificador = ?';
+                $updateQuery = $pdo->prepare($updateSql);
+                $updateQuery->execute([$newPassword, $identificador]);
+
+                // Actualizamos el valor de la contraseña para continuar el proceso de verificación
+                $result['password'] = $newPassword;
+            }
+
+            // Verificar la contraseña usando password_verify
             if (password_verify($password, $result['password'])) {
-                $_SESSION['activeP'] = true;
+                // Almacenamos la sesión del usuario si las credenciales son correctas
+                $_SESSION['active'] = true;
                 $_SESSION['usuario_id'] = $result['usuario_id'];
                 $_SESSION['nombre'] = $result['nombre'];
                 $_SESSION['apellido'] = $result['apellido'];
